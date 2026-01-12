2560f52af16c53d726b89bfbe205fa7388684bc5
satas20 <ata.ayyildiz@outlook.com>
2026-01-12 22:23:02 +0300

    add new mappings etc

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,24 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
--- -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
 local map = vim.keymap.set
 
--- 1. "jj" to Escape in Insert Mode
--- This lets you exit insert mode quickly without reaching for ESC
+-- 1. Escape Mapping
+-- Exit insert mode quickly by typing 'jj'
 map("i", "jj", "<Esc>", { desc = "Quick Escape" })
 
--- 2. Turkish Keyboard Optimizations
--- Map "ö" to start of line (0) and "ç" to end of line ($)
--- This makes navigation much faster on TR keyboards
-map("n", "ö", "0", { desc = "Go to Start of Line" })
-map("n", "ç", "$", { desc = "Go to End of Line" })
-map("v", "ö", "0", { desc = "Go to Start of Line" })
-map("v", "ç", "$", { desc = "Go to End of Line" })
+-- 2. Start/End Line Optimizations
+-- ö and ç are physical equivalents of 0 and $ on your layout
+map({ "n", "v" }, "ö", "0", { desc = "Go to Start of Line" })
+map({ "n", "v" }, "ç", "$", { desc = "Go to End of Line" })
 
--- Optional: If you ever need to type literal ö or ç in Normal mode (rare),
--- you might want to map the original 0/$ keys to them, but usually not needed for nav.
+-- 3. Block Jumps (Paragraphs)
+-- We map these as the primary keys for jumping between code blocks
+-- Note: You can still type ğ/ü in Insert mode for your Turkish comments.
+map({ "n", "o", "x" }, "ğ", "{", { desc = "Jump up paragraph" })
+map({ "n", "o", "x" }, "ü", "}", { desc = "Jump down paragraph" })
+-- Exit terminal mode using Esc (Note: This might interfere with some terminal TUI apps)
+
+-- Exit terminal mode using your "jj" shortcut (Highly Recommended)
+---- Exit terminal mode using your "jj" shortcut
+map("t", "jj", [[<c-\><c-n>]], { desc = "exit terminal mode" })
--- nvim/.config/nvim/lua/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/plugins/copilot-chat.lua
@@ -1,13 +1,59 @@
 return {
+  -- 1. Configure Copilot Chat (Space + a + i)
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    keys = {
+      -- Unmap default keys so they don't conflict
+      { "<leader>aa", false },
+      { "<leader>ac", false },
+
+      -- Set custom key: Space + a + i
+      {
+        "<leader>ai",
+        function()
+          return require("CopilotChat").toggle()
+        end,
+        desc = "Copilot Chat",
+        mode = { "n", "v" },
+      },
+    },
     opts = {
+      suggestions = { enable = true },
       mappings = {
-        -- Unmap the "reset" command so Ctrl-l works for navigation
+        -- Fix: Unmap 'reset' so Ctrl+l works for window navigation
         reset = {
           normal = "",
           insert = "",
         },
+      },
+    },
+  },
+
+  -- 2. Configure Claude / Avante (Space + a + c)
+  {
+    "yetone/avante.nvim",
+    keys = {
+      -- Unmap Avante defaults
+      { "<leader>aa", false },
+
+      -- Set custom key: Space + a + c
+      {
+        "<leader>ac",
+        function()
+          require("avante.api").toggle()
+        end,
+        desc = "Claude Chat (Avante)",
+        mode = { "n", "v" },
+      },
+    },
+  },
+
+  -- 3. Organize the Menu Labels
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>a", group = "AI Assistant", icon = "🤖" },
       },
     },
   },
